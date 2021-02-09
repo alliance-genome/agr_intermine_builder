e760ebfff2b09309cb1a6afa6f12594420e1d6a5
Kalpana Karra <kkarra@stanford.edu>
2021-02-08 20:52:25 -0800

    clean

--- intermine_builder/build.sh
+++ intermine_builder/build.sh
@@ -145,8 +145,8 @@ echo "$(date +%Y/%m/%d-%H:%M) Gradle: build userDB" #>> /home/intermine/intermin
 ./gradlew buildUserDB --stacktrace #>> /home/intermine/intermine/build.progress
 
 echo "$(date +%Y/%m/%d-%H:%M) Gradle: build webapp" #>> /home/intermine/intermine/build.progress
-# ./gradlew clean
+./gradlew clean
 #both needed for now..
 #./gradlew cargoDeployRemote
-#sleep 60
+sleep 60
 ./gradlew cargoRedeployRemote  --stacktrace #>> /home/intermine/intermine/build.progress
