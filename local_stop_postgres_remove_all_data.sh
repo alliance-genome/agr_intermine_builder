3581cb6e5dea08763bf3cd521a8ba98dc85d1f20
Christoper Tabone <christabone@gmail.com>
2024-05-06 20:32:17 +0000

    Fixed bugs in removing pgdata directory.

--- local_stop_postgres_remove_all_data.sh
+++ local_stop_postgres_remove_all_data.sh
@@ -7,7 +7,7 @@ PG_DATA_DIR="/home/ec2-user/pg_data"
 if [ $(docker ps -aq -f name=^/agr.local.alliancemine.postgres.server$) ]; then
     echo "Stopping and removing the 'agr.local.alliancemine.postgres.server' container..."
     # Stop the container if it is running
-    docker stop postgreagr.local.alliancemine.postgres.servers
+    docker stop agr.local.alliancemine.postgres.server
     # Remove the container after stopping
     docker rm agr.local.alliancemine.postgres.server
     echo "Container 'agr.local.alliancemine.postgres.server' has been removed."
@@ -18,7 +18,7 @@ fi
 # Check if the data directory exists and remove it
 if [ -d "$PG_DATA_DIR" ]; then
     echo "Removing the PostgreSQL data directory at $PG_DATA_DIR..."
-    rm -rf "$PG_DATA_DIR"
+    sudo rm -rf "$PG_DATA_DIR"
     echo "Data directory has been removed."
 else
     echo "No data directory at $PG_DATA_DIR to remove."
