407ecd65924a90ba6d5ea4350f196e195e4afca5
Christoper Tabone <christabone@gmail.com>
2024-05-09 18:29:31 +0000

    Added alliancemine.properties for bash.

--- local_intermine_builder_bash.sh
+++ local_intermine_builder_bash.sh
@@ -13,6 +13,10 @@ if [ $(docker ps -aq -f name=^/agr.local.intermine_builder$) ]; then
     echo "Removed existing container named 'agr.local.intermine_builder'"
 fi
 
+# Pull secrets file from AWS
+echo "Pulling secrets file from AWS."
+aws secretsmanager get-secret-value --region=us-east-1 --secret-id IntermineLocalPropertiesFile --query SecretString --output text > ./alliancemine.properties
+
 # Run the Docker container with the volume mounted for persistence and drop into a bash shell
 docker run \
   -it \
@@ -20,5 +24,9 @@ docker run \
   --net intermine \
   --rm \
   -v "/data:/root/data" \
+  -v "$(pwd)/alliancemine.properties:/root/alliancemine/alliancemine.properties" \
   "100225593120.dkr.ecr.us-east-1.amazonaws.com/agr_intermine_builder_env:stage" \
   bash
+
+# Optionally remove the secrets file if you want to ensure it's not left on the host
+rm -f ./alliancemine.properties
